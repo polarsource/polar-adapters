a0aa3772c320ab99e50bcce2d5d92cddb5f85780
Emil Widlund <hello@emilwidlund.com>
2025-04-22 11:22:03 +0200

    update READMEs with new products param

--- packages/polar-betterauth/example/src/lib/auth.ts
+++ packages/polar-betterauth/example/src/lib/auth.ts
@@ -4,35 +4,35 @@ import { Polar } from "@polar-sh/sdk";
 import Database from "better-sqlite3";
 
 const client = new Polar({
-  accessToken: process.env["POLAR_ACCESS_TOKEN"] as string,
-  server: "sandbox",
+	accessToken: process.env["POLAR_ACCESS_TOKEN"] as string,
+	server: "sandbox",
 });
 
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [
-    polar({
-      client,
-      createCustomerOnSignUp: true,
-      enableCustomerPortal: true,
-      checkout: {
-        enabled: true,
-        products: [
-          {
-            productId: "e651f46d-ac20-4f26-b769-ad088b123df2",
-            slug: "pro",
-          },
-        ],
-        successUrl: "/?checkout_id={CHECKOUT_ID}",
-      },
-      webhooks: {
-        secret: process.env["POLAR_WEBHOOK_SECRET"] as string,
-        onPayload: async (payload) =>
-          logger.info(JSON.stringify(payload, null, 2)),
-      },
-    }),
-  ],
-  database: new Database("sqlite.db"),
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [
+		polar({
+			client,
+			createCustomerOnSignUp: true,
+			enableCustomerPortal: true,
+			checkout: {
+				enabled: true,
+				products: [
+					{
+						productId: "e651f46d-ac20-4f26-b769-ad088b123df2",
+						slug: "pro",
+					},
+				],
+				successUrl: "/?checkout_id={CHECKOUT_ID}",
+			},
+			webhooks: {
+				secret: process.env["POLAR_WEBHOOK_SECRET"] as string,
+				onPayload: async (payload) =>
+					logger.info(JSON.stringify(payload, null, 2)),
+			},
+		}),
+	],
+	database: new Database("sqlite.db"),
 });
