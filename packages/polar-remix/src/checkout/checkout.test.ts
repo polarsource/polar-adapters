e98bb44af79c0b1fac646a273fc498e8a678deaf
Emil Widlund <hello@emilwidlund.com>
2025-06-23 09:43:25 +0200

    format code

--- packages/polar-remix/src/checkout/checkout.test.ts
+++ packages/polar-remix/src/checkout/checkout.test.ts
@@ -2,64 +2,64 @@
 const mockCustomerPortalUrl = "https://mock-customer-portal-url.com/";
 const mockCheckoutUrl = "https://mock-checkout-url.com/";
 const mockSessionCreate = vi
-  .fn()
-  .mockResolvedValue({ customerPortalUrl: mockCustomerPortalUrl });
+	.fn()
+	.mockResolvedValue({ customerPortalUrl: mockCustomerPortalUrl });
 const mockCheckoutCreate = vi.fn(() => ({ url: mockCheckoutUrl }));
 
 // Mock the module before any imports
 vi.mock("@polar-sh/sdk", async (importOriginal) => {
-  class Polar {
-    customerSessions = {
-      create: mockSessionCreate,
-    };
+	class Polar {
+		customerSessions = {
+			create: mockSessionCreate,
+		};
 
-    checkouts = {
-      create: mockCheckoutCreate,
-    };
-  }
+		checkouts = {
+			create: mockCheckoutCreate,
+		};
+	}
 
-  return {
-    ...(await importOriginal()),
-    Polar,
-  };
+	return {
+		...(await importOriginal()),
+		Polar,
+	};
 });
 
 import { describe, expect, it, vi } from "vitest";
 import { Checkout } from "./checkout";
 
 describe("Checkout middleware", () => {
-  it("should redirect to checkout when products is valid", async () => {
-    const loader = Checkout({});
+	it("should redirect to checkout when products is valid", async () => {
+		const loader = Checkout({});
 
-    // Test Loader Function
-    const response = await loader({
-      request: new Request("http://localhost:3000/?products=mock-product-id"),
-      context: {},
-      params: {},
-    });
+		// Test Loader Function
+		const response = await loader({
+			request: new Request("http://localhost:3000/?products=mock-product-id"),
+			context: {},
+			params: {},
+		});
 
-    expect(response).toBeInstanceOf(Response);
-    expect((response as Response).status).toBe(302);
-    expect((response as Response).headers.get("Location")).toBe(
-      mockCheckoutUrl,
-    );
-  });
+		expect(response).toBeInstanceOf(Response);
+		expect((response as Response).status).toBe(302);
+		expect((response as Response).headers.get("Location")).toBe(
+			mockCheckoutUrl,
+		);
+	});
 
-  it("should return 400 when products is not defined", async () => {
-    const loader = Checkout({
-      accessToken: "mock-access-token",
-    });
+	it("should return 400 when products is not defined", async () => {
+		const loader = Checkout({
+			accessToken: "mock-access-token",
+		});
 
-    const response = await loader({
-      request: new Request("http://localhost:3000/"),
-      context: {},
-      params: {},
-    });
+		const response = await loader({
+			request: new Request("http://localhost:3000/"),
+			context: {},
+			params: {},
+		});
 
-    expect(response).toBeInstanceOf(Response);
-    expect((response as Response).status).toBe(400);
-    expect(await (response as Response).json()).toEqual({
-      error: "Missing products in query params",
-    });
-  });
+		expect(response).toBeInstanceOf(Response);
+		expect((response as Response).status).toBe(400);
+		expect(await (response as Response).json()).toEqual({
+			error: "Missing products in query params",
+		});
+	});
 });
