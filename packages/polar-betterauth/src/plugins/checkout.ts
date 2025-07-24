ce89896eeb56cb45874837549f2d285edd2ae352
valoomba <loomba.vikash@gmail.com>
2025-07-24 11:04:52 -0700

    Update checkout.ts

--- packages/polar-betterauth/src/plugins/checkout.ts
+++ packages/polar-betterauth/src/plugins/checkout.ts
@@ -47,6 +47,13 @@ export const checkout =
 								z.union([z.string(), z.number(), z.boolean()]),
 							)
 							.optional(),
+						allowDiscountCodes: z
+							.string()
+							.toLowerCase()
+							.transform((x) => x === "true")
+							.pipe(z.boolean())
+							.optional(),
+						discountId: z.string().optional(),
 					}),
 				},
 				async (ctx) => {
@@ -100,6 +107,8 @@ export const checkout =
 									}
 								: ctx.body.metadata,
 							customFieldData: ctx.body.customFieldData,
+							allowDiscountCodes: ctx.body.allowDiscountCodes,
+							discountId: ctx.body.discountId
 						});
 
 						const redirectUrl = new URL(checkout.url);
